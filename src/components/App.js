64aabc0873a8ecc06e95c5d9a265ac961a1c93a3
NicholasKoblitz <f9ycn6yfzc@privaterelay.appleid.com>
2023-12-20 00:44:01 +0000

    Reorginzed file structure and set up App.js

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+          <Routes>
+      
+          </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
