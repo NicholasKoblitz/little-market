f22d4f1d066904151ea7bf1066f523316e83ae1e
NicholasKoblitz <68592588+NicholasKoblitz@users.noreply.github.com>
2023-12-21 10:56:52 -0500

    Created ProductCard component

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,4 +1,5 @@
 import {React, useEffect, useState} from "react";
+import ProductCard from "./ProductCard";
 
 
 const Hompage = () => {
---
+++ src/components/ProductCard.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+
+const ProductCard = (key, title, price, category, description, image) => {
+
+
+    return (
+        <div className="ProductCard" id={key}>
+            <div className="ProductCard-Body">
+                <div className="ProductCard-Image">
+                    <img href={image} alt={title}/>
+                </div>
+                <div className="ProductCard-Info">
+                    <div className="ProductCard-Title">
+                        <h4>
+                            {title}
+                        </h4>
+                    </div>
+                    <div className="ProductCard-Price">
+                        <span>
+                            {price}
+                        </span>
+                    </div>
+                    <div className="ProductCard-Category">
+                        <span>
+                            {category}
+                        </span>
+                    </div>
+                    <div className="ProductCard-Description">
+                        <p>
+                            {description}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductCard;
